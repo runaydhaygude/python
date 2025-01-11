fd4a6c040f2a26f52eb6d20e9ed340f38396e94f
runaydhaygude <runaydhaygude97@gmail.com>
2025-01-12 00:14:18 +0530

    sorting

--- main.py
+++ main.py
@@ -156,20 +156,16 @@ print(data_dict)
 
 
 
-# Pie chart
-import matplotlib.pyplot as pyplot
 
-data = [3, 4, 5]
+# Sorting
+def sort_list(unsorted_list):
+    sorted_list = []
+    for i in range(len(unsorted_list)):
+        min_value = min(unsorted_list)
+        sorted_list.append(min_value)
+        unsorted_list.remove(min_value)
+    return sorted_list
 
-pyplot.pie(data)
-pyplot.show() # this blocks the thread
-
-
-# Bubble plotting
-
-x_values = [1, 2, 3, 4, 5, 6, 7]
-y_values = [3000, 6000, 5000, 8000, 11000, 9000, 10000]
-weight = [100, 150, 2000, 200, 400, 300, 250]
-
-pyplot.scatter(x_values, y_values, weight)
-pyplot.show()
+steps = [4, 2, 8]
+sorted_steps = sort_list(steps)
+print(sorted_steps)

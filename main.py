60881ee896ca4d6d12a8d4872ea1254f21af9315
runaydhaygude <runaydhaygude97@gmail.com>
2025-01-11 20:11:52 +0530

    lists

--- main.py
+++ main.py
@@ -3,3 +3,13 @@ print(day)
 
 day = "Monday"
 print(day)
+
+
+days_of_week = ["Sunday" , "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+print(days_of_week)
+
+steps_per_day = [7000, 5500, 10300, 8000, 1200, 2000, 5000]
+print(steps_per_day)
+
+fitness_data = ["Juliana", 7000, 5500, 10300, 8000, 1200, 2000, 5000]
+print(fitness_data)

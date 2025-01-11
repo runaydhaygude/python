410a96b0d3eef5d7ed17864837d270a731137c47
runaydhaygude <runaydhaygude97@gmail.com>
2025-01-11 20:26:42 +0530

    slice list

--- main.py
+++ main.py
@@ -14,11 +14,19 @@ print(days_of_week)
 
 steps_per_day = [7000, 5500, 10300, 8000, 1200, 2000, 5000]
 print(steps_per_day)
+steps_per_day.append(27)
+print(steps_per_day)
+
 
 fitness_data = ["Juliana", 7000, 5500, 10300, 8000, 1200, 2000, 5000]
 print(fitness_data)
+first_index = fitness_data[0]
+print(first_index)
+last_index = fitness_data[7]
+print(last_index)
 
-
+fitness_data.remove("Juliana")
+print(fitness_data)
 
 
 # If else
@@ -45,3 +53,10 @@ for num in numbers:
     maximum_num = num
 
 print(maximum_num)
+
+
+
+# Slice list
+fitness_data = ["Juliana", 7000, 5500, 10300, 8000, 1200, 2000, 5000]
+slice_list = fitness_data[1:3]
+print(slice_list)

5ecdaa677cd9279f3932ebfa2f5219791c51a45b
runaydhaygude <runaydhaygude97@gmail.com>
2025-01-11 20:12:36 +0530

    if-else

--- main.py
+++ main.py
@@ -13,3 +13,15 @@ print(steps_per_day)
 
 fitness_data = ["Juliana", 7000, 5500, 10300, 8000, 1200, 2000, 5000]
 print(fitness_data)
+
+
+num1 = 5
+num2 = 8
+maximum_num = 0
+
+if num1 >= num2:
+    maximum_num = num1
+else:
+    maximum_num = num2
+
+print(maximum_num)

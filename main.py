e0dd7b26297130e4ca4f307c5caa132a6573c707
runaydhaygude <runaydhaygude97@gmail.com>
2025-01-11 20:16:14 +0530

    for loop

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+
+# Variables
 day = "Sunday"
 print(day)
 
@@ -5,6 +7,8 @@ day = "Monday"
 print(day)
 
 
+
+# Lists
 days_of_week = ["Sunday" , "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
 print(days_of_week)
 
@@ -15,6 +19,9 @@ fitness_data = ["Juliana", 7000, 5500, 10300, 8000, 1200, 2000, 5000]
 print(fitness_data)
 
 
+
+
+# If else
 num1 = 5
 num2 = 8
 maximum_num = 0
@@ -23,5 +30,18 @@ if num1 >= num2:
     maximum_num = num1
 else:
     maximum_num = num2
+
+print(maximum_num)
+
+
+
+
+# for loop
+numbers = [5,10,8]
+maximum_num = 0
+# Write your code here
+for num in numbers:
+  if num > maximum_num:
+    maximum_num = num
 
 print(maximum_num)

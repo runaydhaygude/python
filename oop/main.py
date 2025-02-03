7106eca8a43d16c67fdf5ff670d2a1b41113faed
runaydhaygude <runaydhaygude97@gmail.com>
2025-02-03 23:47:28 +0530

    OOP: implementing properties

--- oop/main.py
+++ oop/main.py
@@ -6,3 +6,25 @@ class MyClass:
 
 obj = MyClass()  # creating a MyClass Object
 print(obj)
+
+
+
+class Employee:
+    # defining the properties and assigning them none
+    # you have to initialize or else it won't compile
+    ID = None
+    salary = None
+    department = None
+
+# cerating an object of the Employee class
+Steve = Employee()
+
+# printing properties of Steve - an object of the Employee class
+print("ID =", Steve.ID)
+print("Salary", Steve.salary)
+print("Department:", Steve.department)
+
+
+# assigning values to properties of Steve - an object of the Employee class
+Steve.ID = 3789
+print("ID =", Steve.ID)

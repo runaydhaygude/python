59bb3d2baa79aed8b044e5dafb9964a87497b89c
runaydhaygude <runaydhaygude97@gmail.com>
2025-02-04 00:04:40 +0530

    OOP: class and instance variables

--- oop/main.py
+++ oop/main.py
@@ -76,3 +76,30 @@ print("Mark")
 print("ID :", Mark.ID)
 print("Salary :", Mark.salary)
 print("Department :", Mark.department)
+
+
+
+
+
+# Class and Instance Variables
+
+class Player:
+    teamName = 'Liverpool'      # class variables
+    teamMembers = []
+
+    def __init__(self, name):
+        self.name = name        # creating instance variables
+        self.formerTeams = []
+        self.teamMembers.append(self.name)
+
+
+p1 = Player('Mark')
+p2 = Player('Steve')
+
+print("Name:", p1.name)
+print("Team Members:")
+print(p1.teamMembers)
+print("")
+print("Name:", p2.name)
+print("Team Members:")
+print(p2.teamMembers)
